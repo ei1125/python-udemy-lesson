aecfd8de231155a7a5225bd6ed4a27cde972868c
ei1125 <i.dont.like.monday1125@gmail.com>
2020-07-29 20:54:35 +0900

    8.変数宣言

---
+++ lesson8.py
@@ -0,0 +1,15 @@
+num = 1
+# name = 'Mike'
+# is_ok = True
+
+# print(num, type(num))
+# print(name, type(name))
+# print(is_ok, type(is_ok))
+
+name = '1'
+new_num = int(name)
+print(new_num, type(new_num))
+
+num2: int = 1
+name2: str = '1'
+
