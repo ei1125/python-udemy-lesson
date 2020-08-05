fc24e8b1ef29655d16c822d658dc4a2282a0ce86
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-05 21:19:41 +0900

    27辞書の使い所

---
+++ lesson27.py
@@ -0,0 +1,17 @@
+# 辞書の使い道
+l = [
+  ['apple', 100],
+  ['banana', 200],
+  ['orange', 300],
+]
+
+# ハッシュテーブルの方が早い
+fruits = {
+  'apple': 100,
+  'banana': 200,
+  'orange': 300,
+}
+
+
+print("リスト型", l[0][1])
+print("辞書型", fruits['apple'])
