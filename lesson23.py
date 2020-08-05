606ca912eb117b632eaf10d26863051414b7e3fb
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-05 20:22:17 +0900

    23.タプルの使い所

---
+++ lesson23.py
@@ -0,0 +1,12 @@
+chose_from_two = ('A', 'B', 'C')
+
+answer = []
+# answer.append('A')
+# answer.append('C')
+chose_from_two.append('A')
+chose_from_two.append('C')
+
+print(chose_from_two)
+print(answer)
+
+# タプルに追加が出来ないので間違い防止になる
