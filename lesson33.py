ca629687921bd4fc15b4028b34886f9a5d18d566
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-06 12:28:18 +0900

    33.if文

---
+++ lesson33.py
@@ -0,0 +1,21 @@
+# if文
+
+x = 10
+
+if x < 0:
+    print('negative')
+elif x == 0:
+    print('zero')
+elif x == 10:
+    print('10000000')
+else:
+    print('positive')
+
+
+a = 5
+b = 10
+
+if a > 0:
+  print('a is positive')
+  if b > 0:
+      print('b is positive')
