d001fb255643a832aba7f2809af07b753e76847a
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-08 10:53:35 +0900

    51

---
+++ sec5-flow/lesson51.py
@@ -0,0 +1,22 @@
+# def test_func(x, l=[]):
+#     l.append(x)
+#     return l
+
+def test_func(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
+# y = [1, 2, 3]
+# r = test_func(100, y)
+# print(r)
+
+# y = [1, 2, 3]
+# r = test_func(200, y)
+# print(r)
+
+r = test_func(100)
+print(r)
+# 参照渡しに気を付ける
+r = test_func(100)
+print(r)
