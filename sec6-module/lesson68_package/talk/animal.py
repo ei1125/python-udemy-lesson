b70a7089164f74b8834a3bc2a970db6e57db905d
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-09 23:13:43 +0900

    70

---
+++ sec6-module/lesson68_package/talk/animal.py
@@ -0,0 +1,7 @@
+from lesson68_package.tools import utils
+
+def sing():
+    return 'ga;wrina;sdfa;owiegna'
+
+def cry():
+    return utils.say_twice('ajkba;jweng;a')
---
+++ sec6-module/lesson70.py
@@ -0,0 +1,9 @@
+# from lesson68_package.talk import human
+# from lesson68_package.talk import animal
+from lesson68_package.talk import *
+
+print(animal.sing())
+print(animal.cry())
+
+print(human.sing())
+print(human.cry())
