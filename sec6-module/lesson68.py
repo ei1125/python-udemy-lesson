62f455faf98e48a54a63e630d9fb6067143dffb8
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-09 15:26:12 +0900

    68

---
+++ sec6-module/lesson68.py
@@ -0,0 +1,8 @@
+# import lesson68_package.utils
+from lesson68_package import utils
+# from lesson68_package import utils as u
+# from lesson68_package.utils import say_twice
+
+r = utils.say_twice('hello')
+
+print(r)
