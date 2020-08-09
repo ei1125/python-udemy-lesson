98dd99790694f05124e606fdbc26d2d82da7db56
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-09 23:31:50 +0900

    72

---
+++ sec6-module/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup( 
+    name='python_programming',
+    version='1.0.0',
+    packages=['lesson68_package', 'lesson68_package.talk', 'lesson68_package.tools'],
+    url='https://sakaijunsoccer.appspot.com',
+    license='Free',
+    author='morishita',
+    author_email='',
+    description='Sample package'
+)
+
+# $ python3 setup.py sdist
