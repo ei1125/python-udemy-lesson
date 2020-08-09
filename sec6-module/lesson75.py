b188093ba73a3665fbeefdde7d9e8de5a17af3c0
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-10 00:13:46 +0900

    75

---
+++ sec6-module/lesson75.py
@@ -0,0 +1,13 @@
+# サードパーティーのライブラリ
+
+# PyPIから取得
+# https://pypi.org/
+# $pip install termcolor
+
+from termcolor import colored
+
+print('test')
+
+print(colored('test', 'red'))
+
+# print(help(colored))
