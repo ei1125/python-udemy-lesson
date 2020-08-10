b5eb4b642f6004e54ed9aae4707c4a21857e3120
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-10 19:17:32 +0900

    94

---
+++ sec8-file/lesson93.py
@@ -0,0 +1,19 @@
+# ファイルの読み込み
+
+s ="""\
+AAA
+BBB
+CCC
+DDD
+"""
+
+with open('test.txt', 'r') as f:
+    # print(f.read())
+    while True:
+        # line = f.readline()
+        # print(line, end='')
+        chunk = 2
+        line = f.read(chunk)
+        print(line)
+        if not line:
+            break
