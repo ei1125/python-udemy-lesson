ebb2ab3929d62f0eaae7489853fdbdfcc7051301
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-12 18:40:48 +0900

    169

--- sec14-test/lesson166.py
+++ sec14-test/lesson166.py
@@ -8,16 +8,34 @@ import pytest
 
 import calculation
 
-# def test_add_num_and_double():
-#     cal = calculation.Cal()
-#     assert cal.add_num_and_double(1, 1) == 4
+is_release = True
 
 class TestCal(object):
-    def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        assert cal.add_num_and_double(1, 1) == 4
 
+    @classmethod
+    def setup_class(cls):
+        print('start')
+        cls.cal = calculation.Cal()
+
+    @classmethod
+    def teardown_class(cls):
+        print('end')
+        del cls.cal
+
+    def setup_method(self, method):
+        print('method={}'.format(method.__name__))
+        # self.cal = calculation.Cal()
+
+    def teardown_method(self, method):
+        print('method={}'.format(method.__name__))
+        # del self.cal
+
+    def test_add_num_and_double(self):
+        assert self.cal.add_num_and_double(1, 1) == 4
+    
+    # @pytest.mark.skip(reason='skip!')
+    @pytest.mark.skipif(is_release==True,
+                        reason='skip!')
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
-            cal = calculation.Cal()
-            cal.add_num_and_double('1', '1')
+            self.cal.add_num_and_double('1', '1')
