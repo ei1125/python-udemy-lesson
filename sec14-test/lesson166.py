f231c2c2aca11b44e46bf3618ae3ad09bcdba544
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-12 18:11:16 +0900

    167

--- sec14-test/lesson166.py
+++ sec14-test/lesson166.py
@@ -1,10 +1,10 @@
 # pytest
 
 # $ pip install pytest
-
 # test_ と assert を書けば良い
-
 # $ pytest ファイル名 で実行
+
+import pytest
 
 import calculation
 
@@ -16,3 +16,8 @@ class TestCal(object):
     def test_add_num_and_double(self):
         cal = calculation.Cal()
         assert cal.add_num_and_double(1, 1) == 4
+
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            cal = calculation.Cal()
+            cal.add_num_and_double('1', '1')
