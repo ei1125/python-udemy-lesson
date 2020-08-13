32f40d650b0967a7e715aa1c17f28f8a21b0e77a
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-13 10:49:32 +0900

    175

---
+++ sec14-test/lesson175.py
@@ -0,0 +1,22 @@
+# setuptoolsでtestを実行する
+
+# $ pip install setuptools
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name='python_programming_demo_app',
+#     version='0.0.2',
+#     packages=find_packages(),
+#     # You could use find_packages if setuptools is installed. 
+#     # packages=find_packages(),
+#     package_data={ 'roboter': ['templates/*.txt'] },
+#     url='http://sakaijunsoccer.appspot.com',
+#     license='MIT',
+#     author='jsakai',
+#     author_email='example@example.com',
+#     # You can specify install_requires if setuptools is installed
+#     install_requires=['termcolor==1.1.0'],
+#     description='roboter description',
+#     test_suits='tests',
+# )
