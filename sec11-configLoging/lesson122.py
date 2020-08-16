6cf46c167e0adbd871c6f3b7097554f84ebfb2ae
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-16 15:12:31 +0900

    122.logger

---
+++ sec11-configLoging/lesson122.py
@@ -0,0 +1,13 @@
+# logging logger
+
+import logging
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info('info')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.debug('debug')
