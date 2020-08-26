36590dc7e986969c541a07a4913ff1739d51b390
Morishita Ei <cwemorishita@rrcs-172-254-99-24.nyc.biz.rr.com>
2020-08-26 21:37:24 +0900

    135

---
+++ sec12-database/lesson135.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+# conn = sqlite3.connect('test_sqlite.db')
+# メモリに一時的に保存
+conn = sqlite3.connect(':memory:')
+
+curs = conn.cursor()
+
+curs.execute(
+    'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')
+conn.commit()
+
+curs.execute(
+    'INSERT INTO persons(name) values("Mike")'
+)
+conn.commit()
+
+curs.execute(
+    'INSERT INTO persons(name) values("Nancy")')
+curs.execute(
+    'INSERT INTO persons(name) values("Jun")')
+conn.commit()
+
+curs.execute('UPDATE persons set name = "Michel" WHERE name = "Mike"')
+conn.commit()
+
+curs.execute('DELETE FROM persons WHERE name = "Michel"')
+conn.commit()
+
+curs.execute('SELECT * FROM persons')
+print(curs.fetchall())
+
+curs.close()
+conn.close()
